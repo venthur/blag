af4898f1f63eefe378c8a50a10d778391f5a26c5
Bastian Venthur <venthur@debian.org>
2021-02-13 12:58:02 +0100

    corrected test for datetime

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -34,8 +34,11 @@ def test_convert_markdown_links(input_, expected):
 
 @pytest.mark.parametrize("input_, expected", [
     ('foo: bar', {'foo': 'bar'}),
+    ('foo: those are several words', {'foo': 'those are several words'}),
     ('tags: this, is, a, test\n', {'tags': ['this', 'is', 'a', 'test']}),
-    ('date: 2020-01-01 12:10', {'date': datetime(2020, 1, 1, 12, 10)}),
+    ('tags: this, IS, a, test', {'tags': ['this', 'is', 'a', 'test']}),
+    ('date: 2020-01-01 12:10', {'date':
+                                datetime(2020, 1, 1, 12, 10).astimezone()}),
 ])
 def test_convert_metadata(input_, expected):
     md = markdown_factory()
