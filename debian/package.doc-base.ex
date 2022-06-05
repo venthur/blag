c469b9b591ff652ba0773880fa3af70ee1ec4433
Bastian Venthur <venthur@debian.org>
2022-06-05 21:15:30 +0200

    WIP

---
+++ debian/package.doc-base.ex
@@ -0,0 +1,20 @@
+Document: blag
+Title: Debian blag Manual
+Author: <insert document author here>
+Abstract: This manual describes what blag is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/blag/blag.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/blag/blag.ps.gz
+
+Format: text
+Files: /usr/share/doc/blag/blag.text.gz
+
+Format: HTML
+Index: /usr/share/doc/blag/html/index.html
+Files: /usr/share/doc/blag/html/*.html
