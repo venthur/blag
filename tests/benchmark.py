60bd86ec225ffff8f1a59778a6f285982e355bda
Bastian Venthur <bastian.venthur@flix.com>
2025-01-22 15:17:28 +0100

    finalized benchmark

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,15 +1,24 @@
+"""Benchmark the performance of the blag build command."""
+
+import logging
 import os
+from argparse import Namespace
+
+from pytest import LogCaptureFixture
 
 import blag
 from blag.blag import build
 
 
-def test_performance(args) -> None:
-    FILES = 1000
-    print(f"Generating {FILES} files")
+def test_performance(args: Namespace, caplog: LogCaptureFixture) -> None:
+    """Test performance of the build command."""
+    caplog.set_level(logging.ERROR)
+
+    FILES = 10000
+    print(f"Generating {FILES} markdown files")
     # create random markdown files in the content directory
     with open(os.path.join(blag.__path__[0], "content", "testpage.md")) as fh:
-              markdown = fh.read()
+        markdown = fh.read()
     for i in range(FILES):
         with open(f"content/{i}.md", "w") as f:
             f.write(markdown)
@@ -20,12 +29,9 @@ def test_performance(args) -> None:
     t = time()
     build(args)
     t_first = time() - t
-    print(t_first)
 
     t = time()
     build(args)
     t_second = time() - t
-    print(t_second)
     print(f"First run: {t_first:.2f}s, second run: {t_second:.2f}s")
     print(f"Speedup: {t_first/t_second:.2f}")
-
